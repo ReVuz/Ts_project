a5983f87d9ac0134b5b9adeb2808873fb77e4a71
Edwin Alex Shaji <96735074+edwineas@users.noreply.github.com>
2024-01-27 00:41:34 +0530

    Update Styles.modules.ts

--- src/styles/Styles.modules.ts
+++ src/styles/Styles.modules.ts
@@ -60,14 +60,72 @@ export const MovieShowsWrapper = styled.div`
             justify-content: center;
             align-items: center;
             flex-direction: column;
+            transition: all 0.4s ease-in-out;
 
-            .moivieImage {
+            .movieImage {
                 display: flex;
                 justify-content: center;
                 align-items: center;
                 flex-direction: column;
                 cursor: pointer;
+
+                > img {
+                    border-radius: 10px;
+                    border-bottom-right-radius: 25px;
+                    box-shadow: 1px 1px 10px #000;
+                    padding: 5px;
+                    height: 200px;
+                }
+
+                > span {
+                    border:none;
+                    height: 35px;
+                    width: 35px;
+                    border-radius: 360px;
+                    background-color: #f76429;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    aligh-self: center;
+                    color: #fff;
+                    padding: 5px;
+                    font-size: 15px;
+                    position: relative;
+                    bottom: 20px;
+                }
+            }
+            &:hover {
+                transform: scale(1.04);
             }
         }
+        .movieInfo {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin-top: 0;
+            position: relative;
+            bottom: 10px;
+
+            > h4{
+                margin-bottom: 0;
+                width: 230px;
+                text-align: center;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+
+            > p {
+                margin: 8px 0;
+                color: gray;
+                border-bottom: 1px solid gray;
+            }
+        }
+    }
+
+    .button{
+        text-align: center;
+        border: none;
     }
 `;
