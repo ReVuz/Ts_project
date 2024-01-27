a34952f609878d947be8f96f704e85c3e5e94cb1
ReVuz <revathymr916@gmail.com>
2024-01-27 20:17:25 +0530

    Commit -n

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ import Movies from "./pages/Movies";
 import Popular from "./pages/Popular";
 import TvShows from "./pages/TvShows";
 import { Route, Routes } from "react-router-dom";
+// import { Button } from './components/ui/button';
+// import { DropdownMenu } from './components/ui/dropdown-menu';
+// import { Toggle } from './components/ui/toggle';
 
 function App() {
   return (
     <>
       <Header />
+      {/* <Button />
+      <DropdownMenu />
+      <Toggle /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Movies" element={<Movies />} />
